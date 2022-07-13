5ff647247a30a4c05cceb0d58b7ef25e24a8d7b9
AlexCernikov <cernikov.alexandr.80@gmail.com>
2022-07-13 16:28:33 +0300

    Create Space Section

---
+++ booking-system/src/components/HomeViewComponents/SpaceSectionComponent.vue
@@ -0,0 +1,41 @@
+<template>
+<!-- eslint-disable max-len -->
+ <section class="our-spaces">
+    <div class="container">
+        <h3 class="our-spaces_title">
+            Our Spaces
+        </h3>
+        <p class="our-spaces_text"> Our space is designed to give you a different experience when working with your team or personally </p>
+    </div>
+
+ </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'spaceSectionComponent',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
