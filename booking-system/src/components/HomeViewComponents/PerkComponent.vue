17917a94e4f7dfc4565383fa6e5a4c1bfa890bc7
Diana G <67389035+dianagatcan@users.noreply.github.com>
2022-07-13 21:57:46 +0300

    Define props for PerkComponent

--- booking-system/src/components/HomeViewComponents/PerkComponent.vue
+++ booking-system/src/components/HomeViewComponents/PerkComponent.vue
@@ -1,32 +1,46 @@
 <template>
-    <div class="perk-container">
-        <img alt="table" />
-        <div class="text-container">
-            <h1>10+</h1>
-            <p>alo</p>
-        </div>
+  <div class="perk-container">
+    <img :src="require('@/assets/' + src)" alt="{{alt}}" />
+    <div class="text-container">
+      <h1>{{ header }}</h1>
+      <p>{{ description }}</p>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PerkComponent',
   props: {
-    icon: {
-      src: String,
-        }
-    }
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    header: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  name: 'PerkComponent',
 });
 </script>
 
 <style lang="scss">
 .perk-container {
-    width: 275px;
-    height: 117px;
-    background-color: aquamarine;
+  width: 275px;
+  height: 117px;
+  background-color: aquamarine;
 }
 
-.text-container {}
+.text-container {
+}
 </style>
--- booking-system/src/components/HomeViewComponents/WorkPerksComponent.vue
+++ booking-system/src/components/HomeViewComponents/WorkPerksComponent.vue
@@ -1,34 +1,31 @@
 <template>
-    <div class="workPerks">
-        <div class="ourSuport">
-            <div class="perks">
+  <div class="workPerks">
+    <div class="ourSuport">
+      <div class="perks">
+        <PerkComponent src="table.svg" alt="table" header="10+" description="Shared tables" />
+        <!-- <PerkComponent />
                 <PerkComponent />
                 <PerkComponent />
                 <PerkComponent />
-                <PerkComponent />
-                <PerkComponent />
-                <PerkComponent />
-            </div>
-        </div>
-        <div class="whyWFO">
-            <div class="head">
-                <div class="frame">
-                    <p>Why working from office</p>
-                    <hr>
-                </div>
-                <h2 class="titleWFO">A place for peaceful and productive work</h2>
-                <p>If you have 5+ kids, fluffy dog, slobbery
-                    cat and a turtle you need a
-                    quite place where to work in
-                    order to feed all these creatures
-                    in your house. Also you might need
-                    a good internet speed, some
-                    cups of cofee per day and
-                    periodically a good professional
-                    advice from a colleague...</p>
-            </div>
-        </div>
+                <PerkComponent /> -->
+      </div>
     </div>
+    <div class="whyWFO">
+      <div class="head">
+        <div class="frame">
+          <p>Why working from office</p>
+          <hr />
+        </div>
+        <h2 class="titleWFO">A place for peaceful and productive work</h2>
+        <p>
+          If you have 5+ kids, fluffy dog, slobbery cat and a turtle you need a quite place where to
+          work in order to feed all these creatures in your house. Also you might need a good
+          internet speed, some cups of cofee per day and periodically a good professional advice
+          from a colleague...
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -43,90 +40,90 @@ export default defineComponent({ name: 'WorkPerksComponent', components: { PerkC
 @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap');
 
-$white: #FFFFFF;
+$white: #ffffff;
 $lato: 'Lato', sans-serif;
 $roboto: 'Roboto Condensed', sans-serif;
 
 //Main container
 .workPerks {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    background-color: bisque;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: bisque;
 }
 
 .perks {
-    display: flex;
-    flex-wrap: wrap;
-    width: 550px;
-    height: 350px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 550px;
+  height: 350px;
 }
 
 .textPerks {
-    font-family: $lato;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 16px;
-    line-height: 26px;
-    color: #9A9A9A;
+  font-family: $lato;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #9a9a9a;
 }
 
 //Left container
 .ourSuport {
-    background-color: aliceblue;
-    width: 550px;
-    height: 350px;
+  background-color: aliceblue;
+  width: 550px;
+  height: 350px;
 }
 
 //Right container
 .whyWFO {
-    background-color: #231F20;
-    width: 540px;
-    height: 421px;
+  background-color: #231f20;
+  width: 540px;
+  height: 421px;
 
-    text-align: left;
+  text-align: left;
 }
 
 .head {
-    width: 433px;
-    height: 132px;
-    margin: 54px 54px 0px 53px;
+  width: 433px;
+  height: 132px;
+  margin: 54px 54px 0px 53px;
 }
 
 .frame {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 0px;
-    gap: 15px;
-    width: 307px;
-    height: 30px;
-    color: $white ;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 15px;
+  width: 307px;
+  height: 30px;
+  color: $white;
 }
 
 p {
-    font-family: $lato;
-    color: $white;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 30px;
+  font-family: $lato;
+  color: $white;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 30px;
 }
 
 hr {
-    width: 73px;
-    height: 2px;
-    background: $white ;
+  width: 73px;
+  height: 2px;
+  background: $white;
 }
 
 .titleWFO {
-    color: $white;
-    font-family: $roboto;
-    font-style: normal;
-    font-weight: 700;
-    font-size: 40px;
-    line-height: 47px;
-    margin-top: 8px;
-    margin-bottom: 32px;
+  color: $white;
+  font-family: $roboto;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 47px;
+  margin-top: 8px;
+  margin-bottom: 32px;
 }
 </style>
