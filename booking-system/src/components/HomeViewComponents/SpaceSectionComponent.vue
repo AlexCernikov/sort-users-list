996636374fc998cdde59c643c92d3b1f0d33e623
AlexCernikov <cernikov.alexandr.80@gmail.com>
2022-07-13 17:56:33 +0300

    begin style

--- booking-system/src/components/HomeViewComponents/SpaceSectionComponent.vue
+++ booking-system/src/components/HomeViewComponents/SpaceSectionComponent.vue
@@ -2,10 +2,23 @@
 <!-- eslint-disable max-len -->
  <section class="our-spaces">
     <div class="container">
-        <h3 class="our-spaces_title">
-            Our Spaces
-        </h3>
-        <p class="our-spaces_text"> Our space is designed to give you a different experience when working with your team or personally </p>
+        <div class="our-spaces__top">
+ <h3 class="our-spaces__title">Our Spaces </h3>
+        <p class="our-spaces__text"> Our space is designed to give you a different experience when working with your team or personally </p>
+        </div>
+    <div class="space">
+        <a class="space__item" href="#">
+              <img class=".space__images" src="../../assets/spacesection/image1.png" alt="image">
+             <div class="space__info">
+               <h6 class="space__info-title">
+                 Truffaut literally trust
+                </h6>
+               <p class="space__info-text">
+                Living room furntiture | Chair
+               </p>
+             </div>
+            </a>
+    </div>
     </div>
 
  </section>
@@ -24,18 +37,21 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+
+.container{
+    width: 80%;
+    margin: auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.our-spaces{
+  padding: 55px 0 55px;
+  &__top{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &__title{
+    font-size: 40px;
+    color: #231F20;
+  }
 }
 </style>
--- booking-system/src/views/HomeView.vue
+++ booking-system/src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HeroSectionComponent msg="Welcome  to Your Vue.js + TypeScript App"/>
+    <SpaceSectionComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HeroSectionComponent from '@/components/HomeViewComponents/HeroSectionComponent.vue'; // @ is an alias to /src
+import SpaceSectionComponent from '@/components/HomeViewComponents/SpaceSectionComponent.vue';
 
 export default defineComponent({
   name: 'heroSection',
   components: {
     HeroSectionComponent,
+    SpaceSectionComponent,
   },
 });
 </script>
