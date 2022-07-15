dfd6ef29bd044ad651c8e23743a6734603317626
AlexCernikov <cernikov.alexandr.80@gmail.com>
2022-07-15 15:33:56 +0300

    create faq item component

--- booking-system/src/components/HomeViewComponents/FaqItemComponent.vue
+++ booking-system/src/components/HomeViewComponents/FaqItemComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="faq__item">
-           {{}}
+           
              </div>
 </template>
 
@@ -10,15 +10,13 @@ import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'FaqComponent',
   props: {
-    faq: {
-      type: Array,
+    name: {
+      type: String,
     },
   },
 });
 </script>
 <style scoped lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
 .container{
     max-width: 1100px;
     margin: auto;
