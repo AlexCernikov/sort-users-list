b9328e3fca2e12e94ad9213e9b35360c0604767a
decebrolus <decebrolus@gmail.com>
2022-07-18 10:52:59 +0300

    Edit Component responsiveness and other styles

--- booking-system/src/components/HomeViewComponents/HeroSectionComponent.vue
+++ booking-system/src/components/HomeViewComponents/HeroSectionComponent.vue
@@ -1,65 +1,69 @@
 <template>
-  <div class="container">
-    <div class="top">
+<div class="hero--container__wrapper">
+  <div class="hero--container">
+    <div class="hero--top">
       <h2><span>Book</span> a perfect spot in the office</h2>
-      <p class="hero-top">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      <p class="hero--description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Nemo nihil, placeat asperiores totam consectetur,
         dolorum numquam animi minima, laudantium quis molestias
         saepe quam velit quisquam atque minus assumenda reprehenderit id!</p>
     </div>
-    <div class="bot">
-      <div class="form">
-        <div class="form-name">Find your Space Now</div>
-        <div class="form-question">
-          <div class="left-content">
-            <p>Type</p>
-            <p>Private Space</p>
-          </div>
-          <div class="right-content">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
-              <polyline fill="none" stroke="#FFF" stroke-width="2"
-                        points="7.086 3.174 17.086 13.174 7.086 23.174"
-                        transform="scale(1 -1) rotate(-89 -1.32 0)"/>
-            </svg>
-          </div>
-
+    <div class="hero--bot">
+      <div class="hero-bottom-wrapper">
+        <div class="hero--form__wrapper">
+          <div class="hero--form">
+            <div class="form-name">Find your Space Now</div>
+            <div class="form-question">
+              <div class="left-content">
+                <p>Type</p>
+                <p>Private Space</p>
+              </div>
+              <div class="right-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+                  <polyline fill="none" stroke="#FFF" stroke-width="2"
+                            points="7.086 3.174 17.086 13.174 7.086 23.174"
+                            transform="scale(1 -1) rotate(-89 -1.32 0)"/>
+                </svg>
+              </div>
+            </div>
+            <div class="form-question">
+              <div class="left-content">
+                <p>Date</p>
+                <p>9 Aug</p>
+              </div>
+              <div class="right-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+                  <polyline fill="none" stroke="#FFF" stroke-width="2"
+                            points="7.086 3.174 17.086 13.174 7.086 23.174"
+                            transform="scale(1 -1) rotate(-89 -1.32 0)"/>
+                </svg>
+              </div>
+            </div>
+            <div class="form-question">
+              <div class="left-content">
+                <p>Time</p>
+                <p>9:00 - 18:00</p>
+              </div>
+              <div class="right-content">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
+                  <polyline fill="none" stroke="#FFF" stroke-width="2"
+                            points="7.086 3.174 17.086 13.174 7.086 23.174"
+                            transform="scale(1 -1) rotate(-89 -1.32 0)"/>
+                </svg>
+              </div>
+            </div>
+            <div class="form-submit">Find my Space</div>
+            </div>
         </div>
-        <div class="form-question">
-          <div class="left-content">
-            <p>Date</p>
-            <p>9 Aug</p>
-          </div>
-          <div class="right-content">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
-              <polyline fill="none" stroke="#FFF" stroke-width="2"
-                        points="7.086 3.174 17.086 13.174 7.086 23.174"
-                        transform="scale(1 -1) rotate(-89 -1.32 0)"/>
-            </svg>
-          </div>
-
+        <div class="img-background">
+          <img src="@/assets/900x600_img.png" alt="office_image">
         </div>
-        <div class="form-question">
-          <div class="left-content">
-            <p>Time</p>
-            <p>9:00 - 18:00</p>
-          </div>
-          <div class="right-content">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24">
-              <polyline fill="none" stroke="#FFF" stroke-width="2"
-                        points="7.086 3.174 17.086 13.174 7.086 23.174"
-                        transform="scale(1 -1) rotate(-89 -1.32 0)"/>
-            </svg>
-          </div>
 
-        </div>
-        <div class="form-submit">Find my Space</div>
-      </div>
-      <div class="img-background">
-        <img src="@/assets/900x600_img.png" alt="office_image">
-      </div>
-
+          </div>
     </div>
   </div>
+</div>
+
 </template>
 
 <script lang="ts">
@@ -72,11 +76,18 @@ export default defineComponent({
 
 <style scoped lang="scss">
 img {
-  right: 20%;
-  position:relative;
-  z-index: -1;
+  width: 45vw;
+  min-width: 500px;
+  z-index: -12;
 }
-.container {
+
+.hero--container__wrapper {
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+}
+.hero--container {
+  width: min(1280px, 90%);
   display: flex;
   justify-content: center;
   align-items: center;
@@ -100,7 +111,7 @@ h2 {
   margin-left: 50px;
 }
 
-.hero-top {
+.hero--description {
   margin-top: 0;
   font-family: 'Lato', sans-serif;
   font-style: normal;
@@ -111,41 +122,50 @@ h2 {
   margin-right: 50px;
 }
 
-.top {
+.hero--top {
   display: grid;
   grid-template-columns:50% 50%;
   width: 100%;
   margin-top: 50px;
 }
 
-.bot {
-  display: flex;
+.hero--bot {
   width: 100%;
-  justify-content: flex-end;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.hero-bottom-wrapper {
+  width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: relative;
 }
 
-.form {
-  width: 450px;
-  height: 470px;
+.hero--form__wrapper {
+  width: 100px;
+}
+.hero--form {
+  position: relative;
+  width: 350px;
+  height: 450px;
   background-color: #231F20;
-  position: absolute;
-  top: 13%;
-  right: 60%;
   display: grid;
   font-size: 1.5rem;
   flex-direction: column;
 }
 
-.form > div {
-  width: 70%;
+.hero--form > div {
+  width: 80%;
   background-color: black;
   margin: 15px auto;
   color: rgb(234, 234, 234);
 
 }
 
-.form > div:last-child {
+.hero--form > div:last-child {
   margin-bottom: 40px;
   display: flex;
   justify-content: center;
@@ -155,7 +175,7 @@ h2 {
   font-family: 'Roboto Condensed', sans-serif;
 }
 
-.form > div:first-child {
+.hero--form > div:first-child {
   margin-top: 20px;
   background: none;
   font-size: 2rem;
