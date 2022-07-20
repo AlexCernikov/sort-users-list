4a52fc4a1acca8287ce9c1ebbe6dc7d3a8f16c40
StefanDumi <96797949+StefanDumi@users.noreply.github.com>
2022-07-20 11:36:23 +0300

    Add Bootstrap-vue-3 to main.ts (#26)
    Co-authored-by: Diana G <67389035+dianagatcan@users.noreply.github.com>

--- booking-system/vue.config.js
+++ booking-system/vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://135.181.104.18:8081',
+    proxy: 'http://135.181.104.18.8081'
   },
 });
