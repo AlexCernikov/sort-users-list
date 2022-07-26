53a7158b7318f0dceed498ca93a2ac16a5ca0463
CalincovNicolai <theblackdstels@gmail.com>
2022-07-26 18:24:32 +0300

    Create .eslintrc.js

---
+++ booking-system/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [// add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/vue3-recommended', // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  rules: {
+    // override/add rules settings here, such as:
+    'vue/no-unused-vars': 'warn',
+    'vue/html-quotes': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/html-indent': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/order-in-components': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/mustache-interpolation-spacing': 'off',
+    'vue/max-attributes-per-line': 'off',
+  },
+};
