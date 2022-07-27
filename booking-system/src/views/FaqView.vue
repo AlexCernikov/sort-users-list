8c69a73aab39679772ea7998fe8273a4599b01f6
Diana G <67389035+dianagatcan@users.noreply.github.com>
2022-07-27 12:01:03 +0300

    Feature/Date-picker (#40)
    * Create calendar component
    
    * Separate time in dateFrom - dateTo

--- booking-system/src/views/FaqView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="faqView">
-    <h1>This is the "FAQ" page. Under Development</h1>
-  </div>
-</template>
---
+++ booking-system/src/views/MyBookingsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="myBookings">
+    <CalendarComponent />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import CalendarComponent from '@/components/HomeViewComponents/CalendarComponent.vue';
+
+export default defineComponent({
+  components: { CalendarComponent },
+});
+</script>
+
+<style scoped lang="scss"></style>
